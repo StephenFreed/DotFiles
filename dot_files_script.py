9df7364fc1df0b15036eb10a913c6953b9aeda7c
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-12-25 17:05:57 -0500

    add script

---
+++ dot_files_script.py
@@ -0,0 +1,7 @@
+import shutil
+
+shutil.copyfile('/Users/stephenfreed/.bash_profile', '/Users/stephenfreed/Projects/Dot-Files/.bash_profile')
+shutil.copyfile('/Users/stephenfreed/.bashrc', '/Users/stephenfreed/Projects/Dot-Files/.bashrc')
+shutil.copyfile('/Users/stephenfreed/.vimrc', '/Users/stephenfreed/Projects/Dot-Files/.vimrc')
+shutil.copyfile('/Users/stephenfreed/.zshenv', '/Users/stephenfreed/Projects/Dot-Files/.zshenv')
+shutil.copyfile('/Users/stephenfreed/.zshrc', '/Users/stephenfreed/Projects/Dot-Files/.zshrc')
