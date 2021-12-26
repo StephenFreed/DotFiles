0355c6e09d239f8342b94902a55add09273aaa48
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-12-25 22:04:17 -0500

    script changes

--- dot_files_script.py
+++ dot_files_script.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python
+
 import shutil
+from git.repo import Repo
+import time
 
 shutil.copyfile('/Users/stephenfreed/.bash_profile', '/Users/stephenfreed/Projects/Dot-Files/.bash_profile')
 shutil.copyfile('/Users/stephenfreed/.bashrc', '/Users/stephenfreed/Projects/Dot-Files/.bashrc')
 shutil.copyfile('/Users/stephenfreed/.vimrc', '/Users/stephenfreed/Projects/Dot-Files/.vimrc')
 shutil.copyfile('/Users/stephenfreed/.zshenv', '/Users/stephenfreed/Projects/Dot-Files/.zshenv')
 shutil.copyfile('/Users/stephenfreed/.zshrc', '/Users/stephenfreed/Projects/Dot-Files/.zshrc')
+
+my_repo = Repo('/Users/stephenfreed/Projects/Dot-Files/.git')
+my_repo.git.add('--all')
+time.sleep(3)
+my_repo.git.commit('-m', 'script auto push', author='Stephen Freed')
+time.sleep(3)
+origin = my_repo.remote(name='origin')
+origin.push()
