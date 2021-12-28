62b311fee3808c5a9fc63bf5bd0d6db8cc2162c5
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-12-28 01:13:18 -0500

    script auto push

--- dot_files_script.py
+++ dot_files_script.py
@@ -21,25 +21,18 @@ my_repo = Repo('/users/stephenfreed/projects/dot-files/.git')
 
 
 
-my_repo.git.add(['--all'])
-my_repo.index.commit('script auto push')
-origin = my_repo.remote('origin')
-origin.push()
 
+PATH_OF_GIT_REPO = r'/Users/stephenfreed/Projects/Dot-Files/.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'script auto push'
 
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit('script auto push')
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')   
 
-
-# PATH_OF_GIT_REPO = r'/Users/stephenfreed/Projects/Dot-Files/.git'  # make sure .git folder is properly configured
-# COMMIT_MESSAGE = 'script auto push'
-#
-# def git_push():
-#     try:
-#         repo = Repo(PATH_OF_GIT_REPO)
-#         repo.git.add(update=True)
-#         repo.index.commit(COMMIT_MESSAGE)
-#         origin = repo.remote(name='origin')
-#         origin.push()
-#     except:
-#         print('Some error occured while pushing the code')    
-#
-# git_push()
+git_push()
