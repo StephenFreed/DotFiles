1ddc549090d496491f73fb75a34b9300e41a31fd
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-12-28 01:18:02 -0500

    script auto push

--- dot_files_script.py
+++ dot_files_script.py
@@ -31,7 +31,8 @@ def git_push():
         repo.git.add(update=True)
         repo.index.commit('script auto push')
         origin = repo.remote(name='origin')
-        origin.push()
+        repo.git.push("origin", "HEAD:refs/for/main")
+        # origin.push()
     except:
         print('Some error occured while pushing the code')   
 
