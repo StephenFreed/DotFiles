a799c0c7d88aa79f9d06123a7c64d10e85100a84
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2021-12-28 17:19:01 -0500

    script auto push 28-12-21 17:19:00

--- dot_files_script.py
+++ dot_files_script.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # copies files to dot-files git directory
 shutil.copyfile('/Users/stephenfreed/.bash_profile', '/Users/stephenfreed/Projects/Dot-Files/.bash_profile')
 shutil.copyfile('/Users/stephenfreed/.bashrc', '/Users/stephenfreed/Projects/Dot-Files/.bashrc')
-shutil.copyfile('/Users/stephenfreed/.ideavimrc', '/Users/stephenfreed/Projects/Dot-Files/bashrc')
+shutil.copyfile('/Users/stephenfreed/.ideavimrc', '/Users/stephenfreed/Projects/Dot-Files/.ideavimrc')
 shutil.copyfile('/Users/stephenfreed/.vimrc', '/Users/stephenfreed/Projects/Dot-Files/.vimrc')
 shutil.copyfile('/Users/stephenfreed/.zshenv', '/Users/stephenfreed/Projects/Dot-Files/.zshenv')
 shutil.copyfile('/Users/stephenfreed/.zshrc', '/Users/stephenfreed/Projects/Dot-Files/.zshrc')
