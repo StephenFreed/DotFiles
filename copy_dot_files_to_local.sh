a3255d5ab92a884fb12823bab914c858a983b70a
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-11-11 14:19:09 -0500

    Fri Nov 11 14:19:09 EST 2022: Auto Git Push From Laptop

--- copy_dot_files_to_local.sh
+++ copy_dot_files_to_local.sh
@@ -5,20 +5,21 @@ read -p "Overwrite Files? <y/n> " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
     # copies files from git directory .dot-files/ locally
-    cp ~/.dot_files/.sf_profile ~/.sf_profile
-    cp ~/.dot_files/.commands.cfg ~/.commands.cfg
-    cp ~/.dot_files/.vimrc ~/.vimrc
+    cp ~/dot_files/.sf_profile ~/.sf_profile
+    cp ~/dot_files/.commands.cfg ~/.commands.cfg
+    cp ~/dot_files/.vimrc ~/.vimrc
 
     # heredoc to source .sf_profile
     bash_grep=$(grep -s "~/.sf_profile" ~/.bashrc)
     if [[ -z $bash_grep ]]
     then
-cat << EOF >> ./.bashrc
+cat << EOF >> ~/.bashrc
 if [[ -f ~/.sf_profile ]]
 then
     . ~/.sf_profile
 fi
 EOF
+    . ~/.bashrc
     fi
 
     zsh_grep=$(grep -s "~/.sf_profile" ~/.zshrc)
@@ -30,6 +31,7 @@ then
     . ~/.sf_profile
 fi
 EOF
+    . ~/.zshrc
     fi
 
 else
